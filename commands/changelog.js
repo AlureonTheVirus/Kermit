c4da7f3f2b22156b37e90d5e622a0b8ee696dfff
AlureonTheVirus <82665946+AlureonTheVirus@users.noreply.github.com>
2021-05-05 06:54:15 -0500

    Update changelog.js

--- commands/changelog.js
+++ commands/changelog.js
@@ -1,16 +1,17 @@
 module.exports = {
 	name: 'changelog',
 	description: 'displays changelog', 
-                execute(message, args) {
-                    const changelogEmbed = new Discord.MessageEmbed()
-                        .setColor('#0099ff')
-                        .setTitle('Changelog:')
-                        .setAuthor('Kermit Bot')
-                        .setDescription("'Kermit is constantly being updated, here's the changelog:' - Isaac Stanger")
-                            .addFields({
-                                name: "\u200B",
-                                value: "5/4/2021 - official beta release, file system/command handler overhaul"
-                            }, )
-                            .setTimestamp()
-                            .setFooter('Kermit Bot, Made By: Isaac Stanger (AlureonTheVirus)', 'https://i.imgur.com/jk22m.jpg'); channel.send(changelogEmbed);
-},
+        execute(message, args) {
+              const changelogEmbed = new Discord.MessageEmbed()
+                     .setColor('#0099ff')
+                     .setTitle('Changelog:')
+                     .setAuthor('Kermit Bot')
+                     .setDescription("'Kermit is constantly being updated, here's the changelog:' - Isaac Stanger")
+                         .addFields({
+                         name: "\u200B",
+                         value: "5/4/2021 - official beta release, file system/command handler overhaul"
+                         }, )
+                         .setTimestamp()
+                         .setFooter('Kermit Bot, Made By: Isaac Stanger (AlureonTheVirus)', 'https://i.imgur.com/jk22m.jpg'); channel.send(changelogEmbed);
+	},
+};
