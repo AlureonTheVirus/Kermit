d7b20e61e0b17bc4d3b25545f0d330525a89bd1f
AlureonTheVirus <82665946+AlureonTheVirus@users.noreply.github.com>
2021-05-05 09:22:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -79,7 +79,13 @@ client.on("voiceStateUpdate", (oldState, newState) => {
                 }
 
                 if (command.args && !args.length) {
-                 	// ...
+                    let reply = `You didn't provide any arguments, ${message.author}!`;
+
+	            if (command.usage) {
+	        	reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+	            }
+
+	            return message.channel.send(reply);
                 }
             
                 const { cooldowns } = client;
