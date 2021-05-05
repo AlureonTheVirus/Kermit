c5f16f437a3dc457fb358058aeab933b01dd939a
AlureonTheVirus <82665946+AlureonTheVirus@users.noreply.github.com>
2021-05-05 09:17:28 -0500

    Update changelog.js

--- commands/changelog.js
+++ commands/changelog.js
@@ -9,9 +9,11 @@ module.exports = {
                      .setDescription("'Kermit is constantly being updated, here's the changelog:' - Isaac Stanger")
                          .addFields({
                          name: "\u200B",
-                         value: "5/4/2021 - official beta release, file system/command handler overhaul"
+		         value: "5/5/2021 - Bugfixes: $ping command now dysplays properly, repaired $help command for specific command help"
+                         value: "5/4/2021 - Official beta release, file system/command handler overhaul"
                          }, )
                          .setTimestamp()
-                         .setFooter('Kermit Bot, Made By: Isaac Stanger (AlureonTheVirus)', 'https://i.imgur.com/jk22m.jpg'); channel.send(changelogEmbed);
+                         .setFooter('Kermit Bot, Made By: Isaac Stanger (AlureonTheVirus)', 'https://i.imgur.com/jk22m.jpg');
+		         channel.send(changelogEmbed);
 	},
 };
