7cf745c6ecdd69424d4f1a589952b2a353cea0e9
AlureonTheVirus <82665946+AlureonTheVirus@users.noreply.github.com>
2021-05-05 06:50:44 -0500

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'ping',
 	description: 'Pings Bot, then replies with latency -- for debuging',
 	execute(message, args) {
-		message.channel.send('🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms');
+		message.channel.send(`🏓 Pong! Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
 	},
 };
