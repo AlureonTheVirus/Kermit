67f9310807d645437e16d62e9a3876e089f00c94
AlureonTheVirus <82665946+AlureonTheVirus@users.noreply.github.com>
2021-05-04 20:02:21 -0500

    Update and rename coinflip.txt to coinflip.js

---
+++ commands/coinflip.js
@@ -0,0 +1,16 @@
+module.exports = {
+        name: 'coinflip',
+        description: 'kermit flips a coin',
+        execute(message, args) {
+            const messages = [
+
+                " Heads!",
+
+                " Tails!",
+
+            ];
+
+            const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+
+            message.reply("it's" + randomMessage);
+        },
