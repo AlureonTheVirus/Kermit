15e1b44a8de67bef7acfea771410485830d76af2
AlureonTheVirus <82665946+AlureonTheVirus@users.noreply.github.com>
2021-05-05 09:44:14 -0500

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,9 +1,13 @@
 module.exports = {
 	name: 'kick',
-	description: 'Kick a user from the server.',
+	description: 'Kicks specified user',
+        cooldown: 2,
+	alias: ['boot', 'hammer', 'wack'],
 	guildOnly: true,
 	permissions: 'KICK_MEMBERS',
+        usage: '<member>',
+	args: true,
 	execute(message, args) {
-		// ...
+
 	},
 };
