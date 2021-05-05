ef8f72973a3035c3f1b3348591c5f5bbc2c63290
AlureonTheVirus <82665946+AlureonTheVirus@users.noreply.github.com>
2021-05-05 10:47:39 -0500

    Update changelog.js

--- commands/changelog.js
+++ commands/changelog.js
@@ -3,17 +3,42 @@ module.exports = {
 	description: 'displays changelog', 
         execute(message, args) {
               const changelogEmbed = new Discord.MessageEmbed()
-                     .setColor('#0099ff')
-                     .setTitle('Changelog:')
-                     .setAuthor('Kermit Bot')
-                     .setDescription("'Kermit is constantly being updated, here's the changelog:' - Isaac Stanger")
-                         .addFields({
-                         name: "\u200B",
-		         value: "5/5/2021 - Bugfixes: $ping command now dysplays properly, repaired $help command for specific command help"
-                         value: "5/4/2021 - Official beta release, file system/command handler overhaul"
-                         }, )
-                         .setTimestamp()
-                         .setFooter('Kermit Bot, Made By: Isaac Stanger (AlureonTheVirus)', 'https://i.imgur.com/jk22m.jpg');
-		         channel.send(changelogEmbed);
+                    {
+    "title": "Changelog",
+    
+    "color": 8448256,
+    "footer": {
+      "icon_url": "https://i.imgur.com/9xnt22b.jpg",
+      "text": "Kermit Bot | Designed By: Isaac Stanger (@AlureonTheVirus)"
+    },
+ 
+    "author": {
+      "name": "Kermit - Alureon's Bot",
+      "icon_url": "https://i.imgur.com/9xnt22b.jpg"
+    },
+    "fields": [
+      {
+        "name": "5/5/2021",
+        "value": "- Bugfixes: $ping command now dysplays properly, repaired $help command for specific command help"
+      },
+      {
+        "name": "5/4/2021",
+        "value": "- Official beta release, file system/command handler overhaul"
+      },
+      
+      {
+        "name": "Known Bugs:",
+        "value": "everything is broken, dont remind me. when I get Kermit into a stable version, I'll start updating the buglist.",
+        "inline": true
+      },
+      {
+        "name": "Kermit's Official Discord:",
+        "value": "(link coming soon)",
+        "inline": true
+      }
+    ]
+  }
+}
+channel.send(changelogEmbed);
 	},
 };
