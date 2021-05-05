d3961790544150dd825e3c958519bc051fdf1b66
AlureonTheVirus <82665946+AlureonTheVirus@users.noreply.github.com>
2021-05-05 09:46:06 -0500

    Update role.js

--- commands/role.js
+++ commands/role.js
@@ -1,8 +1,13 @@
 module.exports = {
 	name: 'role',
+	description: 'Gives a role to a certain user',
+        cooldown: 2,
+	alias: ['perms'],
+	guildOnly: true,
+	permissions: 'MANAGE_ROLES',
+        usage: '<member> <roles>',
 	args: true,
-	usage: '<user> <role>',
 	execute(message, args) {
-		// ...
+
 	},
 };
