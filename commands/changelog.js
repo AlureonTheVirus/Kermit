2dd6f8a886746d27a2800bf0cd44059f79e9877f
AlureonTheVirus <82665946+AlureonTheVirus@users.noreply.github.com>
2021-05-06 09:44:05 -0500

    Update changelog.js

--- commands/changelog.js
+++ commands/changelog.js
@@ -13,7 +13,11 @@ module.exports = {
     "name": "Kermit - Alureon's Bot",
     "icon_url": "https://i.imgur.com/9xnt22b.jpg"
   },
-  "fields": [
+  "fields": 
+    {
+      "name": "5/6/2021",
+      "value": "- added music functionality ($play, $pause, $resume, $skip, $queue, $stop, $volume). updated git repo."
+    },
     {
       "name": "5/5/2021",
       "value": "- Bugfixes: $ping command now dysplays properly, repaired $help command for specific command help"
@@ -25,6 +29,11 @@ module.exports = {
     {
       "name": "Known Bugs:",
       "value": "everything is broken, dont remind me. when I get Kermit into a stable version, I'll start updating the buglist.",
+      "inline": true
+    },
+    {
+      "name": "Github repo:",
+      "value": "all of Kermit's source code can be found here: https://github.com/AlureonTheVirus/Kermit",
       "inline": true
     },
     {
