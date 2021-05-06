94d7aca3a5d2546bd9d6d7abfb7fdc5013271ee8
AlureonTheVirus <82665946+AlureonTheVirus@users.noreply.github.com>
2021-05-06 09:11:37 -0500

    Update and rename reactionrole to reactionrole.js

--- commands/reactionrole.js
+++ commands/reactionrole.js
@@ -1,10 +1,10 @@
 module.exports = {
 	name: 'reactionrole',
 	description: 'sets up a reaction role system, WARNING: Reaction role messages become void if Kermit reboots/goes offline, meaning that you will have to use this command again to set up reaction roles when he reboots.',
-  cooldown: 2,
+        cooldown: 2,
 	guildOnly: true,
 	permissions: 'MANAGE_ROLES',
-  usage: '',
+        usage: '',
 	args: true,
 	execute(message, args) {
     
