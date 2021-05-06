c841d1a32b9d6f876c860ad49ae3684905118d9b
AlureonTheVirus <82665946+AlureonTheVirus@users.noreply.github.com>
2021-05-06 15:33:20 -0500

    Update server.js

--- commands/server.js
+++ commands/server.js
@@ -1,8 +1,10 @@
 module.exports = {
 	name: 'server',
 	description: 'gives server info',
-	
+	cooldown: 2,
+	guildOnly: true,
 	export(message) {
                message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+	       
 	},
 };
