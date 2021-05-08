d0b6d0ac0f375313b62284c87c45263014225919
AlureonTheVirus <82665946+AlureonTheVirus@users.noreply.github.com>
2021-05-07 19:38:02 -0500

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -7,7 +7,7 @@ module.exports = {
 	usage: '[command name]',
 	args: true,
 	cooldown: 5,
-	async execute(message, args) {
+	async execute(message, args, client, queue) {
 		const { channel } = message.member.voice;
 		if (!channel) return message.channel.send('I\'m sorry but you need to be in a voice channel to play music!');
 		const permissions = channel.permissionsFor(message.client.user);
