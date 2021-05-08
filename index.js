7db078db6e2ab8eb779b26a658b75a3e04c0b04b
AlureonTheVirus <82665946+AlureonTheVirus@users.noreply.github.com>
2021-05-07 20:27:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ client.on("guildCreate", guild => {
 
 // run whenever a message is sent. . .
 client.on('message', message => {
-
+    console.log(`[${message.author.username}] -- ${message.content}`);
     // twss --------------------------------------------------------------------------------
     if (!message.content.startsWith(prefix) || message.author.bot) {
         if (twssbool === "1") {
@@ -224,6 +224,17 @@ client.on("voiceStateUpdate", (oldState, newState) => {
             }
         }
     }
+// leave channel if nobody is in it
+  if (oldState.channelID !==  oldState.guild.me.voice.channelID || newState.channel)
+    return;
+
+  // otherwise, check how many people are in the channel now
+  if (!oldState.channel.members.size - 1)
+    setTimeout(() => { // if 1 (you), wait five minutes
+      if (!oldState.channel.members.size - 1) // if there's still 1 member,
+         oldState.channel.leave(); // leave
+     }, 600); // (5 min in ms)
+
 });
 
 client.login(config.BOT_TOKEN);
