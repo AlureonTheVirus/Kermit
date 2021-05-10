b6b10f39d58ed745cfde5ede10dc4bbff71f4254
AlureonTheVirus <82665946+AlureonTheVirus@users.noreply.github.com>
2021-05-10 06:28:47 -0500

    Update donate.js

--- commands/donate.js
+++ commands/donate.js
@@ -25,6 +25,6 @@ module.exports = {
                    }
                   ]
                 };
-                channel.send({ embed });
+                message.channel.send({ embed });
         },
 };
