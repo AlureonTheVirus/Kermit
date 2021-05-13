b565ec9ed3b30734e4c9ebf53e5b452c6bba9928
AlureonTheVirus <82665946+AlureonTheVirus@users.noreply.github.com>
2021-05-13 08:45:36 -0500

    Update ticktacktoe.js

--- commands/ticktacktoe.js
+++ commands/ticktacktoe.js
@@ -1,1 +1,10 @@
+const TicTacToe = require('discord-tictactoe');
 
+module.exports = {
+	name: 'ticktacktoe',
+	description: 'kermit plays tick tack toe!',
+	execute(message, args) {
+        new TicTacToe({ language: 'fr', command: '-ttt' })
+        .attach(client);
+  },
+};
