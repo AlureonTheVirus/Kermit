ee264457d96403b49b990bbae5d159a4e5deb361
AlureonTheVirus <82665946+AlureonTheVirus@users.noreply.github.com>
2021-05-13 09:01:06 -0500

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -2,12 +2,22 @@ module.exports = {
 	name: 'kick',
 	description: 'Kicks specified user',
         cooldown: 2,
-	alias: ['boot', 'hammer', 'wack'],
+	alias: ['boot', 'wack'],
 	guildOnly: true,
 	permissions: 'KICK_MEMBERS',
         usage: '<member>',
 	args: true,
 	execute(message, args) {
-
+if (msg.member.hasPermission("KICK_MEMBERS")) {
+    if (msg.members.mentions.first()) {
+        try {
+            msg.members.mentions.first().kick();
+        } catch {
+            msg.reply("I do not have permissions to kick " + msg.members.mentions.first());
+        }
+    } else {
+        msg.reply("You do not have permissions to kick " + msg.members.mentions.first());
+    }
+}
 	},
 };
