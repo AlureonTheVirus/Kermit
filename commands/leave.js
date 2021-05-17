a2b10ab4d877a0a577c5b35f3d1e82cbb5ce2951
AlureonTheVirus <82665946+AlureonTheVirus@users.noreply.github.com>
2021-05-17 06:57:55 -0500

    Update leave.js

--- commands/leave.js
+++ commands/leave.js
@@ -3,8 +3,13 @@ module.exports = {
 	description: 'kermit leaves the server -- can only be used by devs',
 	execute(message, args, dev_ids) {
 
-    
+    	var allowedToUse_dev = false;
+        	dev_ids.forEach(id => {
+        	    if (message.author.id == id)
+      		          allowedToUse_dev = true;
+      		});
 
+        if (allowedToUse_dev) {
                 message.reply("It's been fun! I will be leaving this server in 5 minutes, say your goodbyes (this action cannot be canceled, kermit still has full functionality until the time has passed");
                     console.log('[SCRIPT] preparing to leave server. . .'); setTimeout(() => {
                             console.log('[SCRIPT] leave: WORKING . . .');
@@ -12,5 +17,6 @@ module.exports = {
                                 message.guild.leave(); console.log('[SCRIPT] leave: OK');
                             }, 5 * 60000);
                 return;
+	}
   },
 };
